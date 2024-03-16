68f3aaf9487c7ad8fc8adc6f1a53e7678fea9897
AntoDB <antonindebreuck@gmail.com>
2024-03-16 13:01:35 +0100

    Update script to stop and change color prefab

--- UnityProject/Assets/Script/pause_stops_M1_stockel_ouest.cs
+++ UnityProject/Assets/Script/pause_stops_M1_stockel_ouest.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class pause_stops_M1_stockel_ouest : MonoBehaviour
 {
+    private Animator animator;
+    private GameObject animatedObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Assurez-vous que l'objet animé possède un Animator
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("L'objet ne possède pas de composant Animator !");
+            return;
+        }
+
+        // Obtenez la référence de l'objet animé
+        animatedObject = animator.gameObject;
     }
 
     // Update is called once per frame
@@ -20,5 +32,19 @@ public class pause_stops_M1_stockel_ouest : MonoBehaviour
     {
         //Debug.Log(gameObject.GetInstanceID());
         Debug.Log(s);
+
+        // Arrêtez l'animation
+        animator.enabled = false;
+
+        // Changez la couleur de l'objet en rouge
+        Renderer renderer = animatedObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            Debug.LogError("L'objet ne possède pas de composant Renderer !");
+        }
     }
 }
