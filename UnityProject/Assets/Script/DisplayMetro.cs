b7151cf48735516de530a1569614baf23d2d970a
AntoDB <antonindebreuck@gmail.com>
2024-05-17 15:43:29 +0200

    Add all stop + display metro and Add teleport to curent station
    - Add all stop
    - Display metro and Add teleport to curent station

--- UnityProject/Assets/Script/DisplayMetro.cs
+++ UnityProject/Assets/Script/DisplayMetro.cs
@@ -51,110 +51,207 @@ public class DisplayMetro : MonoBehaviour
         { "8834", 4 }, // 6 Direction Roi Baudouin (platform 2)
         //{ "8471", 5 }, // 6 Direction Elisabeth (platform 1)
         //{ "8472", 5 }, // 6 Direction Elisabeth (platform 2)
+        { "8371", 10 }, // 6 Direction Delacroix (platform 1) (haussement d'épaule)
     };
 
     // Mapping from pointId to animation time (in seconds)
-    private Dictionary<string, float> pointIdToTime = new Dictionary<string, float>
+    private Dictionary<string, Dictionary<string, float>> clipNameToPointIdToTime = new Dictionary<string, Dictionary<string, float>>
     {
-        // 1 Direction Gare de l'ouest
-        { "8151", 2f },
-        { "8141", 3f },
-        { "8131", 3.8f },
-        { "8121", 4.6f },
-        { "8111", 5.5f },
-        { "8101", 6.4f },
-        { "8091", 8f },
-        { "8081", 9f },
-        { "8071", 10f },
-        { "8061", 11f },
-        { "8051", 12.25f },
-        { "8041", 13.4f },
-        { "8031", 13.9f },
-        { "8021", 14.75f },
-        { "8011", 15.75f },
-        { "8271", 16.6f },
-        { "8281", 17.5f },
-        { "8291", 19f },
-        { "8741", 20f },
-        { "8731", 21f },
-        // 1 Direction Stockel
-        { "8742", 1f },
-        { "8292", 2f },
-        { "8282", 3.5f },
-        { "8272", 4.4f },
-        { "8012", 5.3f },
-        { "8022", 6.25f },
-        { "8032", 7f },
-        { "8042", 7.5f },
-        { "8052", 8.4f },
-        { "8062", 9.3f },
-        { "8072", 11f },
-        { "8082", 12f },
-        { "8092", 13f },
-        { "8102", 14.5f },
-        { "8112", 15.4f },
-        { "8122", 16.3f },
-        { "8132", 17.1f },
-        { "8142", 18f },
-        { "8152", 19f },
-        { "8162", 21f },
-        // 5 Direction Erasme
-        { "8251", 1.5f },
-        { "8241", 2.4f },
-        { "8231", 3.1f },
-        { "8221", 3.9f },
-        { "8211", 5f },
-        { "8201", 6f },
-        /*{ "8071", 7f },
-        { "8061", 8f },
-        { "8051", 9.5f },
-        { "8041", 10.5f },
-        { "8031", 11.1f },
-        { "8021", 11.8f },
-        { "8011", 12.8f },
-        { "8271", 13.8f },
-        { "8281", 14.7f },
-        { "8291", 16f },
-        { "8741", 17f },
-        { "8731", 18f },*/
-        { "8721", 19f },
-        { "8711", 20.4f },
-        { "8701", 21.3f },
-        { "8691", 22.1f },
-        { "8681", 22.8f },
-        { "8671", 23.5f },
-        { "8661", 24.4f },
-        { "8651", 25.4f },
-        { "8641", 27f },
-        // 5 Direction Hermann-Debroux
-        { "8652", 1.8f },
-        { "8662", 2.8f },
-        { "8672", 3.7f },
-        { "8682", 4.5f },
-        { "8692", 5.1f },
-        { "8702", 6f },
-        { "8712", 6.8f },
-        { "8722", 8f },
-        /*{ "8732", 9f },
-        { "8742", 10f },
-        { "8292", 11f },
-        { "8282", 12.5f },
-        { "8272", 13.4f },
-        { "8012", 14.3f },
-        { "8022", 15.2f },
-        { "8032", 16f },
-        { "8042", 16.5f },
-        { "8052", 17.4f },
-        { "8062", 18.3f },
-        { "8072", 20f },*/
-        { "8202", 21f },
-        { "8212", 22f },
-        { "8222", 23.2f },
-        { "8232", 24f },
-        { "8242", 24.6f },
-        { "8252", 25.4f },
-        { "8262", 27f },
-        // Add more mappings as needed
+        { "Train_anim_stockel_gareouest", new Dictionary<string, float>
+            {
+                // 1 Direction Gare de l'ouest
+                { "8161", 0f },
+                { "8162", 0f }, // Stockel (platform 2)
+                { "8151", 2f },
+                { "8141", 3f },
+                { "8131", 3.8f },
+                { "8121", 4.6f },
+                { "8111", 5.5f },
+                { "8101", 6.4f },
+                { "8091", 8f },
+                { "8081", 9f },
+                { "8071", 10f },
+                { "8061", 11f },
+                { "8051", 12.25f },
+                { "8041", 13.4f },
+                { "8031", 13.9f },
+                { "8021", 14.75f },
+                { "8011", 15.75f },
+                { "8271", 16.6f },
+                { "8281", 17.5f },
+                { "8291", 19f },
+                { "8741", 20f },
+                { "8731", 21f },
+                { "8732", 21f }, // Gare de l'Ouest (platform 2)
+                { "8733", 21f }, // Gare de l'Ouest (platform 3)
+            }
+        },
+        { "Train_anim_gareouest_stockel", new Dictionary<string, float>
+            {
+                // 1 Direction Stockel
+                { "8731", 0f }, // Gare de l'Ouest (platform 1)    
+                { "8732", 0f },
+                { "8733", 0f }, // Gare de l'Ouest (platform 3)
+                { "8742", 1f },
+                { "8292", 2f },
+                { "8282", 3.5f },
+                { "8272", 4.4f },
+                { "8012", 5.3f },
+                { "8022", 6.25f },
+                { "8032", 7f },
+                { "8042", 7.5f },
+                { "8052", 8.4f },
+                { "8062", 9.3f },
+                { "8072", 11f },
+                { "8082", 12f },
+                { "8092", 13f },
+                { "8102", 14.5f },
+                { "8112", 15.4f },
+                { "8122", 16.3f },
+                { "8132", 17.1f },
+                { "8142", 18f },
+                { "8152", 19f },
+                { "8161", 21f }, // Stockel (platform 1)
+                { "8162", 21f },
+            }
+        },
+        { "Train_anim_hermann-debroux_erasme", new Dictionary<string, float>
+            {
+                // 5 Direction Erasme
+                { "8261", 0f },
+                { "8262", 0f }, // Hermann-Debreoux (platform 2)
+                { "8251", 1.5f },
+                { "8241", 2.4f },
+                { "8231", 3.1f },
+                { "8221", 3.9f },
+                { "8211", 5f },
+                { "8201", 6f },
+                { "8071", 7f },
+                { "8061", 8f },
+                { "8051", 9.5f },
+                { "8041", 10.5f },
+                { "8031", 11.1f },
+                { "8021", 11.8f },
+                { "8011", 12.8f },
+                { "8271", 13.8f },
+                { "8281", 14.7f },
+                { "8291", 16f },
+                { "8741", 17f },
+                { "8731", 18f },
+                { "8721", 19f },
+                { "8711", 20.4f },
+                { "8701", 21.3f },
+                { "8691", 22.1f },
+                { "8681", 22.8f },
+                { "8671", 23.5f },
+                { "8661", 24.4f },
+                { "8651", 25.4f },
+                { "8641", 27f },
+                { "8642", 0f }, // Erasme (platform 2)
+            }
+        },
+        { "Train_anim_erasme_hermann-debroux", new Dictionary<string, float>
+            {
+                // 5 Direction Hermann-Debroux
+                { "8641", 0f }, // Erasme (platform 1)
+                { "8642", 0f },
+                { "8652", 1.8f },
+                { "8662", 2.8f },
+                { "8672", 3.7f },
+                { "8682", 4.5f },
+                { "8692", 5.1f },
+                { "8702", 6f },
+                { "8712", 6.8f },
+                { "8722", 8f },
+                { "8732", 9f },
+                { "8742", 10f },
+                { "8292", 11f },
+                { "8282", 12.5f },
+                { "8272", 13.4f },
+                { "8012", 14.3f },
+                { "8022", 15.2f },
+                { "8032", 16f },
+                { "8042", 16.5f },
+                { "8052", 17.4f },
+                { "8062", 18.3f },
+                { "8072", 20f },
+                { "8202", 21f },
+                { "8212", 22f },
+                { "8222", 23.2f },
+                { "8232", 24f },
+                { "8242", 24.6f },
+                { "8252", 25.4f },
+                { "8261", 27f }, // Hermann-Debreoux (platform 1)
+                { "8262", 27f },
+            }
+        },
+        { "Train_anim_elisabeth_roiBaudouin", new Dictionary<string, float>
+            {
+                // 2 & 6 Direction Simonis or Roi Baudouin
+                { "8471", 0f },
+                { "8472", 0f }, // Elisabeth (platform 2)
+                { "8461", 0f },
+                { "8441", 0f },
+                { "8431", 0f },
+                { "8421", 0f },
+                { "8411", 0f },
+                { "8401", 0f },
+                { "8301", 0f },
+                { "8311", 0f },
+                { "8321", 0f },
+                { "8331", 3f },
+                { "8341", 3.8f },
+                { "8351", 4.6f },
+                { "8361", 5.5f },
+                { "8371", 6.4f },
+                { "8381", 6.4f },
+                { "8743", 8f },
+                { "8753", 9f },
+                { "8763", 10f },
+                { "8773", 11f },
+                { "8783", 12.25f },
+                { "8793", 13.4f },
+                { "8803", 13.9f },
+                { "8813", 14.75f },
+                { "8823", 15.75f },
+                { "8833", 0f },
+                { "8834", 16.6f }, // Roi Baudouin (platform 2)
+            }
+        },
+        { "Train_anim_roiBaudouin_elisabeth", new Dictionary<string, float>
+            {
+                // 2 & 6 Direction Elisabeth
+                { "8833", 0f }, // Roi Baudouin (platform 1)
+                { "8834", 0f },
+                { "8824", 0f },
+                { "8814", 0f },
+                { "8804", 0f },
+                { "8794", 0f },
+                { "8784", 0f },
+                { "8774", 0f },
+                { "8764", 0f },
+                { "8763", 10f }, // Simonis -> Elisabeth
+                { "8754", 0f },
+                { "8744", 3.8f },
+                { "8382", 4.6f },
+                { "8372", 5.5f },
+                { "8362", 6.4f },
+                { "8352", 8f },
+                { "8342", 9f },
+                { "8332", 10f },
+                { "8322", 11f },
+                { "8312", 12.25f },
+                { "8302", 13.4f },
+                { "8402", 13.9f },
+                { "8412", 14.75f },
+                { "8422", 15.75f },
+                { "8432", 16.6f },
+                { "8442", 17.5f },
+                { "8462", 17.5f },
+                { "8471", 0f }, // Elisabeth (platform 1)
+                { "8472", 17.5f },
+            }
+        },
     };
 
     // Coroutine to fetch data from STIB API
@@ -284,22 +381,27 @@ public class DisplayMetro : MonoBehaviour
                     {
                         string clipName = clipInfos[0].clip.name;
                         Debug.Log("clipInfo " + clipInfos + " ClipName " + clipName);
-                        if (clipName == "Train_anim_stockel_gareouest" || clipName == "Train_anim_gareouest_stockel" || clipName == "Train_anim_hermann-debroux_erasme" || clipName == "Train_anim_erasme_hermann-debroux" || clipName == "Train_anim_elisabeth_roiBaudouin" || clipName == "Train_anim_roiBaudouin_elisabeth")
+                        Dictionary<string, float> pointIdToTime;
+                        if (clipNameToPointIdToTime.TryGetValue(clipName, out pointIdToTime))
                         {
-                            isAnimationSet = true;
-                            AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
-                            float animationLength = state.length;
                             float animationTime;
                             if (pointIdToTime.TryGetValue(vehicle.pointID, out animationTime))
                             {
+                                AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+                                float animationLength = state.length;
                                 float normalizedTime = animationTime / animationLength;
                                 Debug.Log($"Animation clip name: {clipName}, length: {animationLength}, Animation time: {animationTime}, Normalized time: {normalizedTime}");
                                 animator.Play(state.fullPathHash, -1, normalizedTime);
+                                isAnimationSet = true;
                             }
                             else
                             {
                                 Debug.LogWarning("Unknown pointID: " + vehicle.pointID);
                             }
+                        }
+                        else
+                        {
+                            Debug.LogWarning("Unknown clipName: " + clipName);
                         }
                     }
                     yield return null;
